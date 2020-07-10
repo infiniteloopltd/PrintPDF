dd56469cd52a1eac7d9f21af1578aaa05123fe06
fiach <fiach.reid@gmail.com>
2020-07-10 12:42:40 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Spire.Pdf;
+using System.Drawing.Printing;
+
+namespace PrintPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iSelection = 1;
+            Console.WriteLine("Which printer do you wish to use:");
+            foreach (string printer in PrinterSettings.InstalledPrinters)
+            {
+                Console.WriteLine(iSelection + ":" + printer);
+                iSelection++;
+            }
+            iSelection = Convert.ToInt16(Console.ReadLine());
+            var printerName = PrinterSettings.InstalledPrinters[--iSelection];
+            // Install-Package Spire.PDF -Version 6.7.6
+            // http://www.joyprinter.com/index.html -- handy for development
+            var document = new PdfDocument();
+            document.LoadFromFile("hello-world.pdf");
+            document.PrintSettings.PrinterName = printerName;
+            document.Print();
+            document.Dispose();
+        }
+    }
+}
